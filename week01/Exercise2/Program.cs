64a731ef4973973919a56f8c98b1396b45d32760
JoaoGuilherme7 <jgam3010@gmail.com>
2025-05-10 15:45:52 -0300

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter, message;
+        float grade;
+
+        Console.Write("Enter your grade: ");
+        grade = float.Parse(Console.ReadLine());
+
+        if      (grade >= 90)   letter = "A";
+        else if (grade >= 80)   letter = "B";
+        else if (grade >= 70)   letter = "C";
+        else if (grade >= 60)   letter = "D";
+        else                    letter = "F";
+
+        if (grade >= 70)    message = "Congratulations! You passed the course.";
+        else                message = "Sorry, you failed the course. Keep trying, I believe in you!";
+
+        if (grade <= 93 && letter != "F")
+            if      (grade % 10 >= 7) letter += "+";
+            else if (grade % 10 <= 3) letter += "-";
+
+
+        Console.WriteLine(message);
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
