1de799132ff785fca7cce1b33c4477f9a17d1509
JoaoGuilherme7 <jgam3010@gmail.com>
2025-05-11 01:09:05 -0300

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string again = "1";
+        int magic, number, attempts;
+
+        while (again == "1")
+        {
+            Console.Write("What is the magic number? ");
+            magic = int.Parse(Console.ReadLine());
+            number = magic + 1;
+            attempts = 0;
+
+            while (number != magic)
+            {
+                Console.Write("Number: ");
+                number = int.Parse(Console.ReadLine());
+
+                if      (number > magic)    Console.WriteLine("Lower");
+                else if (number < magic)    Console.WriteLine("Higher");
+
+                attempts++;
+            }
+            Console.WriteLine($"Congratulations, you made it in {attempts} attempts.");
+            Console.Write("Type 1 to play again: ");
+            again = Console.ReadLine();
+        }
     }
 }
