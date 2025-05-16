53c2b857a29cd5571fb74d485c07a2a2c10ba20e
JoaoGuilherme7 <jgam3010@gmail.com>
2025-05-16 02:56:47 -0300

    Correct smallest condition

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -20,7 +20,7 @@ class Program
             number = int.Parse(Console.ReadLine());
             numbers.Add(number);
             if (number > largest) largest = number;
-            if ((number < smallest && smallest > 0) || (smallest <0)) smallest = number;
+            if (number > 0 && number < smallest) smallest = number;
         }
 
         numbers.RemoveAt(numbers.Count - 1);
@@ -37,7 +37,7 @@ class Program
         Console.WriteLine($"Average: {(float)sum / numbers.Count} ");
         Console.WriteLine($"Largest number {largest}");
 
-        if(smallest > 0 )   Console.WriteLine($"The smallest positive is : {smallest}");
-        else                Console.WriteLine("There's no positive numbers. ");
+        if (smallest > 0) Console.WriteLine($"The smallest positive is : {smallest}");
+        else Console.WriteLine("There's no positive numbers. ");
     }
 }
