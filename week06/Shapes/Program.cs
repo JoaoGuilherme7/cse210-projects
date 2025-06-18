1984f9275e75f35fffef1d12d52cedcb0055f1f1
JoaoGuilherme7 <jgam3010@gmail.com>
2025-06-18 19:19:11 -0300

    Commiting Shapes assignment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("green", 3));
+        shapes.Add(new Rectangle("red", 4, 5));
+        shapes.Add(new Circle("blue", 7));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
+
     }
 }
