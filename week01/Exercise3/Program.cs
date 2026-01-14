932f9ea4eae6c3fcac0a80c2bca3963b29ac7f54
João Guilherme Aguiar Moreira <jgam3010@gmail.com>
2026-01-14 01:09:50 -0300

    feat: magic number exercise completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string again = "1";
+        int magic, number, attempts;
+
+        while (again == "1")
+        {
+            magic = new Random().Next(1, 101);
+            Console.WriteLine("\nGuess the number between 1 and 100\n");
+            number = magic + 1;
+            attempts = 0;
+
+            while (number != magic)
+            {
+                Console.Write("Number: ");
+                number = int.Parse(Console.ReadLine());
+
+                if      (number > magic)    Console.WriteLine("Lower");
+                else if (number < magic)    Console.WriteLine("Higher");
+
+                attempts++;
+            }
+            Console.WriteLine($"Congratulations, you made it in {attempts} attempts.");
+            Console.Write("Type 1 to play again: ");
+            again = Console.ReadLine();
+        }
     }
 }
